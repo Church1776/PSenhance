55b75cee69b60effc1043adde43dc5999a51d379
Church1776 <brassmajor@gmail.com>
2026-07-27 16:05:16 -0400

    Updated tab indention inconsistency within repo. New standard will be tabs only with 2 space indentation.

--- Enhancements/Windows/tools.ps1
+++ Enhancements/Windows/tools.ps1
@@ -1,5 +1,5 @@
 if (-not ($Programs -or $LocalUserPrograms)) {
-    . "$PSScriptRoot\environment\paths.ps1"
+	. "$PSScriptRoot\environment\paths.ps1"
 }
 
 $LLVM = "$Programs\LLVM"
@@ -14,21 +14,21 @@ $vcpkg = "$Programs\vcpkg"
 $vcpkg_Local = "$LocalUserPrograms\vcpkg"
 
 $Paths = @(
-    "$LLVM\bin",
-    "$LLVM_Local\bin",
-    "$CMake\bin",
-    "$CMake_Local\bin", 
-    "$Git\cmd",
-    "$Git_Local\cmd",
-    "$Ninja\bin",
-    "$Ninja_Local\bin",
-    "$vcpkg\bin",
-    "$vcpkg_Local\bin"
+	"$LLVM\bin",
+	"$LLVM_Local\bin",
+	"$CMake\bin",
+	"$CMake_Local\bin", 
+	"$Git\cmd",
+	"$Git_Local\cmd",
+	"$Ninja\bin",
+	"$Ninja_Local\bin",
+	"$vcpkg\bin",
+	"$vcpkg_Local\bin"
 )
 
 foreach ($path in $Paths) {
-    if (-not ($env:PATH -split ';' | Where-Object { $_ -eq "$path" }) -and (Test-Path "$path")) {
-        $env:PATH += ";$path"
-        Write-Host "Adding Tool: $($path.Replace($env:USERPROFILE, '~'))"
-    }
+	if (-not ($env:PATH -split ';' | Where-Object { $_ -eq "$path" }) -and (Test-Path "$path")) {
+		$env:PATH += ";$path"
+		Write-Host "Adding Tool: $($path.Replace($env:USERPROFILE, '~'))"
+	}
 }
--- psenhance.ps1
+++ psenhance.ps1
@@ -1,4 +1,3 @@
-
 $name ??= 'cerulean'
 $AT ??= 'brightblue'
 $machine ??= 'skybluebright'
@@ -15,14 +14,14 @@ $colon ??= 'gray'
 $OS_Enhancements = $env:OS
 
 if ($OS_Enhancements -eq "Windows_NT") {
-    $OS_Enhancements = "$($OS_Enhancements.Substring(0,7))"
+	$OS_Enhancements = "$($OS_Enhancements.Substring(0,7))"
 }
 
 $enhancements = @(Get-ChildItem -Path "$PSScriptRoot\Enhancements\$OS_Enhancements" -File *".ps1")
 $enhancements += @(Get-ChildItem -Path "$PSScriptRoot\Enhancements" -File *".ps1")
 
 foreach ($enhancement in $enhancements) {
-    . "$enhancement"
+	. "$enhancement"
 }
 
 $script_location = $($MyInvocation.MyCommand.Path)
@@ -30,57 +29,50 @@ $script_location = $script_location.Replace("$env:USERPROFILE", '~')
 
 Write-Host "Loaded PSenhancement Script: $script_location"
 function prompt {
-    if (-not $global:gitRepoRoot -or -not $PWD.Path.StartsWith($global:gitRepoRoot)) {
-        $global:gitRepoRoot = ""
-        if ($gitBranch -ne "") {
-            $gitBranch = ""
-        }
-        if ($global:gitHashLength -ne 0) {
-            $global:gitHashLength = 0
-        }
-        $tempPWD = $(Get-Location).Path
-        while ($tempPWD) {
-            if (Test-Path "$tempPWD\.git") {
-                $global:gitRepoRoot = $tempPWD
-                break
-            }
-            $tempPWD = (Get-Item $tempPWD).Parent.FullName
-        }
-    }
-    if ("$global:gitRepoRoot" -ne "") {
-        $gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
-        if ($gitBranch -match 'ref: refs/heads/(.+)') {
-            $gitBranch = $matches[1]
-        } else {
-            $gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
-            if (-not $global:gitHashLength) {
-                $gitShortHashId = "$(git rev-parse --short HEAD 2>$null)"
-                $global:gitHashLength = $gitShortHashId.Length
-            }
-            $gitBranch = "HEAD$($ink[$vcs_2nd])@$($ink[$vcs_3rd])$($gitBranch.Substring(0, $global:gitHashLength))$($ink[$vcs_main])"
-        }
-        if ($gitBranch) {
-            $gitBranch = "$($ink[$vcs_main]):${gitBranch}:$($ink.reset) "
-        }
-    }
-    
-    #########################  Keeping if I decide to fallback to the below method of getting the git branch name.  #########################
-    #
-    #if ( ($gitBranch = "$(git rev-parse --abbrev-ref HEAD 2>$null)")) {
-    #    $gitBranch = "$($ink.gray):${gitBranch}:$($ink.reset) "
-    #}
-    #
-    $currentPath = "$($PWD.Path)" # Remove the drive letter and colon (e.g., "C:")
+	if (-not $global:gitRepoRoot -or -not $PWD.Path.StartsWith($global:gitRepoRoot)) {
+		$global:gitRepoRoot = ""
+		if ($gitBranch -ne "") {
+			$gitBranch = ""
+		}
+		if ($global:gitHashLength -ne 0) {
+			$global:gitHashLength = 0
+		}
+		$tempPWD = $(Get-Location).Path
+		while ($tempPWD) {
+			if (Test-Path "$tempPWD\.git") {
+				$global:gitRepoRoot = $tempPWD
+				break
+			}
+			$tempPWD = (Get-Item $tempPWD).Parent.FullName
+		}
+	}
+	if ("$global:gitRepoRoot" -ne "") {
+		$gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
+		if ($gitBranch -match 'ref: refs/heads/(.+)') {
+			$gitBranch = $matches[1]
+		} else {
+			$gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
+			if (-not $global:gitHashLength) {
+				$gitShortHashId = "$(git rev-parse --short HEAD 2>$null)"
+				$global:gitHashLength = $gitShortHashId.Length
+			}
+			$gitBranch = "HEAD$($ink[$vcs_2nd])@$($ink[$vcs_3rd])$($gitBranch.Substring(0, $global:gitHashLength))$($ink[$vcs_main])"
+		}
+		if ($gitBranch) {
+			$gitBranch = "$($ink[$vcs_main]):${gitBranch}:$($ink.reset) "
+		}
+	}
 
-    if ("$currentPath" -eq "$($env:USERPROFILE)" -or $currentPath.StartsWith("$($env:USERPROFILE)")) {
-        $currentPath = $currentPath.Replace("$($env:USERPROFILE)", '~')
-    }
+	$currentPath = "$($PWD.Path)" # Remove the drive letter and colon (e.g., "C:")
 
-    if ("$currentPath" -cmatch "^[A-Za-z]:[/\\]") {
-        $currentDrive = "$($ink[$drv_letter])$($currentPath.Substring(0,2))" # Highlight the drive a different color if it exists.
-        $currentPath = "$($currentPath.Substring(2))" # Shift the path to remove the drive portion from the path.
-    }
+	if ("$currentPath" -eq "$($env:USERPROFILE)" -or $currentPath.StartsWith("$($env:USERPROFILE)")) {
+		$currentPath = $currentPath.Replace("$($env:USERPROFILE)", '~')
+	}
 
-    "$($ink[$name])$env:USERNAME$($ink[$AT])@$($ink[$machine])$env:COMPUTERNAME$($ink[$colon]):$($ink[$system_env])Windows$($ink[$colon]):$currentDrive$($ink[$win32_path])$currentPath$($ink[$win32_Z])>$($ink.reset) $gitBranch"
-    #"$($ink.cerulean)$env:USERNAME$($ink.brightblue)@$($ink.skybluebright)$env:COMPUTERNAME$($ink.gray):$($ink.gold)Windows$($ink.gray):$($ink.periwinkle)PS$($ink.gray):$($ink.amber)$($PWD.Drive.Name)$($ink.amber):$($ink.coral)$currentPath$($ink.brightred)>$($ink.reset) $gitBranch"
+	if ("$currentPath" -cmatch "^[A-Za-z]:[/\\]") {
+		$currentDrive = "$($ink[$drv_letter])$($currentPath.Substring(0,2))" # Highlight the drive a different color if it exists.
+		$currentPath = "$($currentPath.Substring(2))" # Shift the path to remove the drive portion from the path.
+	}
+
+	"$($ink[$name])$env:USERNAME$($ink[$AT])@$($ink[$machine])$env:COMPUTERNAME$($ink[$colon]):$($ink[$system_env])Windows$($ink[$colon]):$currentDrive$($ink[$win32_path])$currentPath$($ink[$win32_Z])>$($ink.reset) $gitBranch"
 }
