6602fb886d273d699e2a588149e2f38359a41535
Church1776 <brassmajor@gmail.com>
2026-08-20 23:39:13 -0400

    Updated windows tools script. Still a work in progress. Adding for future work on other devices.

--- Enhancements/Windows/tools.ps1
+++ Enhancements/Windows/tools.ps1
@@ -1,13 +1,14 @@
 
-$Tools ??= @(
-	"CMake",
-	"Git",
-	"LLVM",
-	"Lua",
-	"Ninja",
-	"PreMake",
-	"VCPkg"
-)
+$Tools ??= [ordered]@{
+	"7-Zip" = ""
+	"CMake" = "bin"
+	"Git" = "cmd"
+	"LLVM" = "bin"
+	"Lua" = "bin"
+	"Ninja" = ""
+	"PreMake" = "bin"
+	"VCPkg" = "bin"
+}
 $Paths ??= @(
 	"$env:ProgramFiles",
 	"${env:ProgramFiles(X86)}",
@@ -16,17 +17,53 @@ $Paths ??= @(
 )
 
 $ToolPaths += $(Get-PSDrive -PSProvider FileSystem).Name | ForEach-Object { "${_}:\Tools" }
-
 $Paths += $ToolPaths
 
-foreach ($tool in $Tools) {
+function Evaluate-ToolPath {
+	param(
+		[Parameter(Mandatory = $true)]
+		[string]$FilePath,
+		[Parameter(Mandatory = $true)]
+		[string]$FolderPath
+	)
+	if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
+		if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
+			if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
+				if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * * * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
+					return $null
+				} else {
+					$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * * * "$FolderPath")).Path
+				}
+			} else {
+				$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * * "$FolderPath")).Path
+			}
+		} else {
+			$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * "$FolderPath")).Path
+		}
+	} else {
+		$FilePath = $(Join-Path "$FilePath" "$FolderPath")
+	}
+	return $FilePath
+}
+foreach ($tool in $Tools.Keys) {
 	foreach ($path in $Paths) {
-		$toolPath = Join-Path "$path" "$tool"
-		if ( -not (Test-Path "$toolPath")) {
+		$toolPath = $(Resolve-Path $(Join-Path "$path" "$tool") -ErrorAction SilentlyContinue).Path
+		if ( -not (Test-Path -Path "$toolPath" -ErrorAction SilentlyContinue)) {
 			continue
 		}
-		if (Test-Path $(Join-Path "$toolPath" "bin")) {
-			$toolPath = $(Join-Path "$toolPath" "bin")
+		$subFolder = $Tools[$tool]
+		if (-not ($null -eq $subFolder -or $subFolder -eq "")) {
+			$toolPath = $(Evaluate-ToolPath -FilePath $toolPath -FolderPath $subFolder)
+		} elseif (-not (Test-Path -Path $(Join-Path "$toolPath" "*.exe") -ErrorAction SilentlyContinue)) {
+			$toolPath = $(Evaluate-ToolPath -FilePath $toolPath -FolderPath "bin")
+			if (-not (Test-Path -Path $(Join-Path "$toolPath" "*.exe") -ErrorAction SilentlyContinue)) {
+				$toolPath = $null
+			}
+		} else {
+			$toolPath = $(Resolve-Path "$toolPath").Path
+		}
+		if ($null -eq $toolPath -or $toolPath -eq "") {
+			continue
 		}
 		if (-not ($env:PATH -split ';' | Where-Object { $_ -eq "$toolPath" }) -and (Test-Path "$toolPath")) {
 			$env:PATH = "$toolPath;$env:PATH"
