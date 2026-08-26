f4136f34689b17f7b5469b4eba4e8590467727d7
Church1776 <brassmajor@gmail.com>
2026-08-26 02:41:01 -0400

    Updated tool discovery. Made searching more efficient and robust by utilizing Get-ChildItem instead of piecing paths together.

--- Enhancements/Windows/tools.ps1
+++ Enhancements/Windows/tools.ps1
@@ -1,13 +1,13 @@
 
 $Tools ??= [ordered]@{
-	"7-Zip" = ""
-	"CMake" = "bin"
-	"Git" = "cmd"
-	"LLVM" = "bin"
-	"Lua" = "bin"
-	"Ninja" = ""
-	"PreMake" = "bin"
-	"VCPkg" = "bin"
+	"7-Zip" = "7z"
+	"CMake" = "cmake"
+	"Git" = "cmd/git"
+	"LLVM" = "clang"
+	"Lua" = "lua"
+	"Ninja" = "ninja"
+	"PreMake" = "premake"
+	"VCPkg" = "vcpkg"
 }
 $Paths ??= @(
 	"$env:ProgramFiles",
@@ -19,55 +19,32 @@ $Paths ??= @(
 $ToolPaths += $(Get-PSDrive -PSProvider FileSystem).Name | ForEach-Object { "${_}:\Tools" }
 $Paths += $ToolPaths
 
-function Evaluate-ToolPath {
-	param(
-		[Parameter(Mandatory = $true)]
-		[string]$FilePath,
-		[Parameter(Mandatory = $true)]
-		[string]$FolderPath
-	)
-	if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
-		if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
-			if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
-				if (-not (Test-Path -Path $(Resolve-Path $(Join-Path "$FilePath" * * * "$FolderPath") -ErrorAction SilentlyContinue) -ErrorAction SilentlyContinue)) {
-					return $null
-				} else {
-					$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * * * "$FolderPath")).Path
-				}
-			} else {
-				$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * * "$FolderPath")).Path
-			}
-		} else {
-			$FilePath = $(Resolve-Path $(Join-Path "$FilePath" * "$FolderPath")).Path
-		}
-	} else {
-		$FilePath = $(Join-Path "$FilePath" "$FolderPath")
-	}
-	return $FilePath
-}
 foreach ($tool in $Tools.Keys) {
 	foreach ($path in $Paths) {
-		$toolPath = $(Resolve-Path $(Join-Path "$path" "$tool") -ErrorAction SilentlyContinue).Path
-		if ( -not (Test-Path -Path "$toolPath" -ErrorAction SilentlyContinue)) {
+		if (($null -eq $path -or $path -eq "") -or ($null -eq $tool -or $tool -eq "")) {
 			continue
 		}
-		$subFolder = $Tools[$tool]
-		if (-not ($null -eq $subFolder -or $subFolder -eq "")) {
-			$toolPath = $(Evaluate-ToolPath -FilePath $toolPath -FolderPath $subFolder)
-		} elseif (-not (Test-Path -Path $(Join-Path "$toolPath" "*.exe") -ErrorAction SilentlyContinue)) {
-			$toolPath = $(Evaluate-ToolPath -FilePath $toolPath -FolderPath "bin")
-			if (-not (Test-Path -Path $(Join-Path "$toolPath" "*.exe") -ErrorAction SilentlyContinue)) {
-				$toolPath = $null
-			}
-		} else {
-			$toolPath = $(Resolve-Path "$toolPath").Path
-		}
-		if ($null -eq $toolPath -or $toolPath -eq "") {
+		if (-not (Test-Path -Path "$path" -ErrorAction SilentlyContinue)) {
 			continue
 		}
+
+		$toolDirectory = "$(Get-ChildItem -Path "$path" -Filter "$tool" -Directory -Recurse -Depth 1 | Select-Object -First 1)"
+		if (-not (Test-Path -Path "$toolDirectory" -ErrorAction SilentlyContinue)) {
+			continue
+		}
+
+		$toolSubFolder = "$(Split-Path "$($Tools[$tool])" -Parent)"
+		$toolExecutable = "$(Split-Path "$($Tools[$tool])" -Leaf).exe"
+		if ( -not (("$null" -eq "$toolSubFolder") -or ("$toolSubFolder" -eq ""))) {
+			$toolDirectory = "$(Get-ChildItem -Path "$toolDirectory" -Filter "$toolSubFolder" -Directory -Recurse | Select-Object -First 1)"
+		}
+		$toolExecutable = "$(Get-ChildItem -Path "$toolDirectory" -Filter "$toolExecutable" -File -Recurse | Select-Object -First 1)"
+
+		$toolPath = "$(Split-Path -Path "$($toolExecutable)" -Parent)"
+		
 		if (-not ($env:PATH -split ';' | Where-Object { $_ -eq "$toolPath" }) -and (Test-Path "$toolPath")) {
 			$env:PATH = "$toolPath;$env:PATH"
-			Write-Host "Adding Tool: $($toolPath.Replace($env:USERPROFILE, '~'))"
+			Write-Host "Adding ToolPath: $($toolPath.Replace($env:USERPROFILE, '~'))"
 		}
 	}
 }
