7bca34772ce915669c5d3245bc676ed4a41eb909
Church1776 <brassmajor@gmail.com>
2026-09-10 02:17:21 -0400

    Updated aliases. Removed Unix style and cmd.exe aliases. Changed which and where to locate and lookup to further distinguish pwsh to prevent bad interpretation of cmdlets and pwsh usage.

--- Enhancements/command-aliases.ps1
+++ Enhancements/command-aliases.ps1
@@ -1,15 +1,104 @@
-Remove-Alias -Name where -Force
 
-Set-Alias -Name nd -Value mkdir -Force -ErrorAction SilentlyContinue
-Set-Alias -Name rd -Value rmdir -Force -ErrorAction SilentlyContinue
-Set-Alias -Name wo -Value Where-Object -Force -ErrorAction SilentlyContinue
-Set-Alias -Name read -Value Read-Host -Force -ErrorAction SilentlyContinue
-Set-Alias -Name tpa -Value Test-Path -Force -ErrorAction SilentlyContinue
-Set-Alias -Name split -Value Split-Path -Force -ErrorAction SilentlyContinue
-Set-Alias -Name join -Value Join-Path -Force -ErrorAction SilentlyContinue
+$UneededAliases = @(
+	"cat"
+	"cd"
+	"cp"
+	"echo"
+	"kill"
+	"ls"
+	"mv"
+	"ps"
+	"pwd"
+	"rm"
+	"type"
 
-function which {
+	"chdir"
+	"cls"
+	"del"
+	"diff"
+	"dir"
+
+	"copy"
+	"cvpa"
+	"erase"
+	"h"
+	"popd"
+	"pushd"
+	"r"
+	"rvpa"
+)
+
+$RestructuredAliases = @{
+	nd = "mkdir"
+	rd = "rmdir"
+	wh = "Write-Host"
+	ral = "Remove-Alias"
+	popl = "Pop-Location"
+	pushl = "Push-Location"
+	get = "Get-Process"
+	out = "Out-Default"
+	read = "Read-Host"
+	convert = "Convert-Path"
+	join = "Join-Path"
+	resolve = "Resolve-Path"
+	split = "Split-Path"
+	stop = "Stop-Process"
+	test = "Test-Path"
+}
+
+ForEach ($a in $UneededAliases) {
+	Remove-Alias -Name $a  -Force
+}
+
+ForEach ($entry in $RestructuredAliases.GetEnumerator()) {
+	$aliasName = $entry.Key
+	$cmdName = $entry.Value
+	if ($null -eq $cmdName) {
+		continue
+	}
+	if (-not (Get-Alias -Name $aliasName -ErrorAction SilentlyContinue)) {
+		Set-Alias -Name $aliasName -Value $cmdName -Force -Scope Global -ErrorAction SilentlyContinue
+	}
+}
+
+function locate {
+	$longUnixFormFlag = 0
+
+	$helpMessage = 
+	"Usage: locate [options] <command>
+	Options:
+	  --help    Display this help message"
+	$versionMessage = 
+	"Locate Utility - Version 0.1.0"
 	foreach ($arg in $args) {
+		if ( $arg -like "-*" -or $arg -like "/*") {
+			$chars = $arg.ToCharArray()
+			foreach ($c in $chars) {
+				if ($c -eq '-') {
+					if ($longUnixFormFlag -eq 2) {
+						Write-Error "Unknown option passed."
+						Write-Error "Usage: locate [options] <command>"
+						return
+					}
+					$longUnixFormFlag = $longUnixFormFlag + 1
+					continue
+				}
+				if ($longUnixFormFlag -eq 2) {
+					break
+				}
+				switch ($c) {
+					{ $c -eq 'h'} { "$helpMessage"; return }
+					{ $c -eq 'v'} { "$versionMessage"; return }
+					default      { break }
+				}
+			}
+			switch ($arg.Substring(2)) {
+				{ $_ -like "help"} { "$helpMessage"; return }
+				{ $_ -like "version"} { "$versionMessage"; return }
+				default      { break }
+			}
+		}
+
 		$err = $null
 		Get-Command -Syntax $arg -ErrorAction SilentlyContinue -ErrorVariable err
 		| ForEach-Object { $_ -replace '\\{2,}', '\' -replace '//{2,}', '/' }
@@ -18,8 +107,58 @@ function which {
 		}
 	}
 }
-function where {
+function lookup {
+	$longUnixFormFlag = 0
+	$nonUnixFormFlag = 0
+
+	$helpUnixStyleMessage = 
+	"Usage: lookup [options] <command>
+	Options:
+	  -h, --help    	Display this help message
+	  -v, --version 	Display the version information"
+	$helpNonUnixStyleMessage = 
+	"Usage: lookup [options] <command>
+	Options:
+	  /?, /h, /help 	Display this help message"
+	$versionMessage = 
+	"Lookup Utility - Version 0.1.0"
 	foreach ($arg in $args) {
+		if ( $arg -like "-*" -or $arg -like "/*") {
+			$chars = $arg.ToCharArray()
+			foreach ($c in $chars) {
+				if ($c -eq '/' -and $FormFlag -eq 0) {
+					if ($nonUnixFormFlag -eq 1) {
+						Write-Error "Unknown option '$arg' passed."
+						Write-Host "$helpNonUnixStyleMessage"
+						return
+					}
+					$nonUnixFormFlag = 1
+					continue
+				} elseif ($c -eq '-' -and $longUnixFormFlag -eq 0) {
+					if ($longUnixFormFlag -eq 2) {
+						Write-Error "Unknown option '$arg' passed."
+						Write-Host "$helpNonUnixStyleMessage"
+						return
+					}
+					$longUnixFormFlag = $longUnixFormFlag + 1
+					continue
+				}
+				if ($longUnixFormFlag -eq 2) {
+					break
+				}
+				switch ($c) {
+					{ $c -eq 'h'} { "$helpUnixStyleMessage"; return }
+					{ $c -eq 'v'} { "$versionMessage"; return }
+					default      { break }
+				}
+			}
+			switch ($arg.Substring(2)) {
+				{ $_ -like "help"} { "$helpUnixStyleMessage"; return }
+				{ $_ -like "version"} { "$versionMessage"; return }
+				default      { break }
+			}
+		}
+
 		$err = $null
 		Get-Command -Syntax -All $arg -ErrorAction SilentlyContinue -ErrorVariable err
 		| ForEach-Object { $_ -replace '\\{2,}', '\' -replace '//{2,}', '/' }
@@ -89,6 +228,7 @@ function add_utility_aliases {
 	}
 }
 
+return
 add_utility_aliases $llvmHashTable
 add_utility_aliases $clangHashTable
 add_utility_aliases $mlirHashTable
--- psenhance.ps1
+++ psenhance.ps1
@@ -32,39 +32,77 @@ foreach ($enhancement in $enhancements) {
 $script_location = $($MyInvocation.MyCommand.Path)
 $script_location = $script_location.Replace("$env:USERPROFILE", '~')
 
+$vcsRoot = ""
+$vcsData = ""
+$vcsBranch = ""
+$vcsHashLength = ""
+$vcsCmdRerun = ""
+$cachedVCS = "git"
+$cachedDirectory = ""
+
+function Set-VCS {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$VCS
+	)
+
+	if (-not ($VCS -in @("git", "fossil", "svn"))) {
+		Write-Host "Invalid VCS specified. Supported values are: git, fossil, svn."
+	}
+	$global:cachedVCS = $VCS
+}
+
 function prompt {
-	if (-not $global:gitRepoRoot -or -not $PWD.Path.StartsWith($global:gitRepoRoot)) {
-		$global:gitRepoRoot = ""
-		if ($gitBranch -ne "") {
-			$gitBranch = ""
-		}
-		if ($global:gitHashLength -ne 0) {
-			$global:gitHashLength = 0
-		}
-		$tempPWD = $(Get-Location).Path
-		while ($tempPWD) {
-			if (Test-Path "$tempPWD\.git") {
-				$global:gitRepoRoot = $tempPWD
+	if (($(Get-History -Count 1).CommandLine -match '(?m)(?:^|[;|&({])\s*git(?:\s|$)') -and ( -not ("$global:vcsRoot" -like ""))) {
+		#write-host "Setting vcsCmdRerun..."
+		$global:vcsCmdRerun = 1
+	}
+	if ($PWD -ne $cachedDirectory) {
+		#write-host "Checking directory..."
+		if ((("$PWD\" -like "$global:vcsRoot\*") -and $(Test-Path "$PWD\.git") -and ( -not ("$PWD\.git" -like "$global:vcsRoot\.git"))) -or ( -not ("$PWD\" -like "$global:vcsRoot\*"))) {
+			#write-host "Looking for repository..."
+			$global:vcsRoot = ""
+			$global:vcsData = ""
+			$global:vcsBranch = ""
+			$global:vcsHashLength = ""
+			$tempPWD = (Get-Item $PWD)
+			while ($tempPWD) {
+				if (-not (Test-Path "$($tempPWD.FullName)\.git")) {
+					$tempPWD = $tempPWD.Parent
+					continue
+				}
+				$global:vcsRoot = $tempPWD.FullName
 				break
 			}
-			$tempPWD = (Get-Item $tempPWD).Parent.FullName
+		}
+		if ($global:vcsData -eq "" -and $global:vcsRoot -ne "") {
+			#write-host "Entering repository..."
+			if (Test-Path -Type Leaf "$($global:vcsRoot)\.git") {
+				#write-host "Locating repo metadata..."
+				$global:vcsData = $(Get-Content "$($global:vcsRoot)\.git")
+				$global:vcsData = $global:vcsData.Trim("gitdir: ")
+			} else {
+				#write-host "Caching metadata Location..."
+				$global:vcsData = "$($global:vcsRoot)\.git"
+			}
+			$global:vcsHashLength = $((git rev-parse --short HEAD 2>$null).Length)
+			if ($global:vcsCmdRerun -eq "") {
+				$global:vcsCmdRerun = 1
+			}
 		}
 	}
-	if ("$global:gitRepoRoot" -ne "") {
-		$gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
-		if ($gitBranch -match 'ref: refs/heads/(.+)') {
-			$gitBranch = $matches[1]
+	if ("$global:vcsData" -ne "" -and "$global:vcsCmdRerun" -eq 1) {
+		#write-host "Reading repo metadata..."
+		$global:vcsCmdRerun = ""
+		$global:vcsBranch = "$(Get-Content "$($global:vcsData)\HEAD")"
+		if (-not ($global:vcsBranch -match 'ref: refs/heads/(.+)')) {
+			#write-host "Finding detached HEAD..."
+			$global:vcsBranch = "HEAD$($ink[$vcs_2nd])@$($ink[$vcs_3rd])$($global:vcsBranch.Substring(0, $global:vcsHashLength))$($ink[$vcs_main])"
 		} else {
-			$gitBranch = "$(Get-Content "$($global:gitRepoRoot)\.git\HEAD")"
-			if (-not $global:gitHashLength) {
-				$gitShortHashId = "$(git rev-parse --short HEAD 2>$null)"
-				$global:gitHashLength = $gitShortHashId.Length
-			}
-			$gitBranch = "HEAD$($ink[$vcs_2nd])@$($ink[$vcs_3rd])$($gitBranch.Substring(0, $global:gitHashLength))$($ink[$vcs_main])"
+			#write-host "Resolving branch name..."
+			$global:vcsBranch = $global:vcsBranch -replace 'ref: refs/heads/', ''
 		}
-		if ($gitBranch) {
-			$gitBranch = "$($ink[$vcs_main]):${gitBranch}:$($ink.reset) "
-		}
+		$global:vcsBranch = "$($ink[$vcs_main]):${global:vcsBranch}:$($ink.reset) "
 	}
 
 	$currentPath = "$($PWD.Path)" # Remove the drive letter and colon (e.g., "C:")
@@ -77,6 +115,6 @@ function prompt {
 		$currentDrive = "$($ink[$drv_letter])$($currentPath.Substring(0,2))" # Highlight the drive a different color if it exists.
 		$currentPath = "$($currentPath.Substring(2))" # Shift the path to remove the drive portion from the path.
 	}
-
-	"$($ink[$name])$env:USERNAME$($ink[$AT])@$($ink[$machine])$env:COMPUTERNAME$($ink[$colon]):$($ink[$system_env])Windows$($ink[$colon]):$currentDrive$($ink[$win32_path])$currentPath$($ink[$win32_Z])>$($ink.reset) $gitBranch"
+	$global:cachedDirectory = $PWD
+	"$($ink[$name])$env:USERNAME$($ink[$AT])@$($ink[$machine])$env:COMPUTERNAME$($ink[$colon]):$($ink[$system_env])Windows$($ink[$colon]):$currentDrive$($ink[$win32_path])$currentPath$($ink[$win32_Z])>$($ink.reset) $vcsBranch"
 }
